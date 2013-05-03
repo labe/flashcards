379550c71276498be0b5db5a5bf278a926a622f0
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-03 11:14:05 -0500

    Changes to View and Dealer classes

--- flashcards.rb
+++ flashcards.rb
@@ -48,38 +48,47 @@ class Dealer
   
   def run 
     @viewer.print_welcome
-    @viewer.print_question
-    check_answer(gets.chomp.upcase)
-    
-  end
-  
-  def check_answer(guess)
-    if 
-
-end   
-
-class Viewer
-  def initialize
-  end
-  
-  def print_welcome
-    
+    until @deck.finished?
+      print_question
+      check_answer(gets.chomp)
+    end
+    @viewer.print_exit    
   end
 
   def print_question
-    
+    @viewer.print_question(@deck.deck[0].question)
+  end
+
+  def check_answer(guess)
+    if @deck.deck[0].is_correct?(guess.upcase)
+      @viewer.print_correct
+      @deck.next_question
+    else
+      @viewer.print_incorrect
+      @viewer.print_question
+    end
+  end
+end   
+
+class Viewer
+  def print_welcome
+    puts "Welcome to Ruby Flashcards!"  
+  end
+
+  def print_question(question)
+    puts "Definition:\n#{question}"
   end
   
   def print_correct
-    
+    puts "That's Correct!"
   end
 
   def print_incorrect
-    
+    puts "Try again..."
   end
 
   def print_exit
-    
+    puts "Thank you for playing."
   end
 
 end
