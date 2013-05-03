a014a589caecdaaa7f94efc3174ec3840b470b3a
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-03 10:56:39 -0500

    added all files to main dir

---
+++ flashcards.rb
@@ -0,0 +1,87 @@
+class Deck
+  attr_reader :deck
+
+  def initialize(file)
+     @file = file
+     @deck = []     
+     import_deck
+  end
+
+  def import_deck
+    @deck << 
+  end
+
+  def shuffle
+    
+  end
+
+  def next_question
+    
+  end
+
+  def finished?
+    
+  end
+  
+  
+end
+
+class Flashcard
+  attr_reader :question, :answer
+
+  def initialize(question, answer)
+    @question = question
+    @answer = answer
+  end
+  
+  def is_correct?(guess)
+    true if guess == @answer
+  end
+  
+end
+
+class Dealer
+  def initialize(file)
+    @deck = Deck.new(file)
+    @viewer = Viewer.new
+  end
+  
+  def run 
+    @viewer.print_welcome
+    @viewer.print_question
+    check_answer(gets.chomp.upcase)
+    
+  end
+  
+  def check_answer(guess)
+    if 
+
+end   
+
+class Viewer
+  def initialize
+  end
+  
+  def print_welcome
+    
+  end
+
+  def print_question
+    
+  end
+  
+  def print_correct
+    
+  end
+
+  def print_incorrect
+    
+  end
+
+  def print_exit
+    
+  end
+
+end
+
+Dealer.new(ARGV[0]).run
